73c8b0f4fc295999275fffb255ba9df9d1f46b31
omkarpradhan12 <42708220+omkarpradhan12@users.noreply.github.com>
2022-11-06 16:33:13 +0530

    shuffeling metadata

--- src/structure/post_list/post_list.component.ts
+++ src/structure/post_list/post_list.component.ts
@@ -40,11 +40,31 @@ export class Post_listComponent implements OnInit {
     this.metadata.push(doc.data())
     // console.log(doc.id,"=>",doc.data());
   });
+
+  this.shuffle(this.metadata)
   //console.log(this.metadata[0].url)
   //return metadata
 
   }
 
+
+  shuffle(array:any) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
 
 
 }
